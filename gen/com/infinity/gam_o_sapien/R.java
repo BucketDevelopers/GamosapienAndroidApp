3487f9a8fe1410c558b7f0be27a45995f78d7c63
master-atul <atulanand94@gmail.com>
2014-10-04 18:19:50 -0700

    Added UI for changing Keys

--- gen/com/infinity/gam_o_sapien/R.java
+++ gen/com/infinity/gam_o_sapien/R.java
@@ -1081,13 +1081,14 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
-        public static final int About=0x7f050053;
-        public static final int IPaddress=0x7f05003f;
-        public static final int Port=0x7f050040;
-        public static final int Supported=0x7f05003c;
-        public static final int a4=0x7f050049;
-        public static final int a8=0x7f05004d;
-        public static final int accelsupport=0x7f050042;
+        public static final int About=0x7f05005a;
+        public static final int Description=0x7f05003c;
+        public static final int IPaddress=0x7f050042;
+        public static final int Port=0x7f050043;
+        public static final int Supported=0x7f05003f;
+        public static final int a4=0x7f05004c;
+        public static final int a8=0x7f050050;
+        public static final int accelsupport=0x7f050045;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1099,41 +1100,44 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050051;
+        public static final int action_settings=0x7f050058;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
-        public static final int b1=0x7f050046;
-        public static final int b2=0x7f050047;
-        public static final int b3=0x7f050048;
-        public static final int b4=0x7f05004a;
-        public static final int b5=0x7f05004b;
-        public static final int b6=0x7f05004c;
-        public static final int b7=0x7f05004e;
-        public static final int b8=0x7f05004f;
-        public static final int b9=0x7f050050;
+        public static final int b1=0x7f050049;
+        public static final int b2=0x7f05004a;
+        public static final int b3=0x7f05004b;
+        public static final int b4=0x7f05004d;
+        public static final int b5=0x7f05004e;
+        public static final int b6=0x7f05004f;
+        public static final int b7=0x7f050051;
+        public static final int b8=0x7f050052;
+        public static final int b9=0x7f050053;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
+        public static final int dialogButtonCancel=0x7f050057;
+        public static final int dialogButtonOK=0x7f050056;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
-        public static final int editKeys=0x7f050052;
+        public static final int editKeys=0x7f050059;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
+        public static final int enteredKey=0x7f050055;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
-        public static final int forSupported=0x7f05003d;
-        public static final int forUnsupported=0x7f050041;
+        public static final int forSupported=0x7f050040;
+        public static final int forUnsupported=0x7f050044;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int keyPadTableLayout=0x7f050044;
+        public static final int keyPadTableLayout=0x7f050047;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int magsupport=0x7f050043;
+        public static final int magsupport=0x7f050046;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1151,7 +1155,7 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
-        public static final int sendIP=0x7f05003e;
+        public static final int sendIP=0x7f050041;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1159,7 +1163,10 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int tableRow1=0x7f050045;
+        public static final int tableRow1=0x7f050048;
+        public static final int team=0x7f05003d;
+        public static final int teammembers=0x7f05003e;
+        public static final int text=0x7f050054;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1219,12 +1226,14 @@ containing a value of this type.
         public static final int about=0x7f030018;
         public static final int activity_main=0x7f030019;
         public static final int controlscreenlayout=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int custom=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
     }
     public static final class string {
+        public static final int Gamosapien_Description=0x7f0a0025;
         public static final int IP_details_label=0x7f0a0017;
         public static final int Supported=0x7f0a0010;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1288,6 +1297,8 @@ containing a value of this type.
         public static final int magnetometer=0x7f0a0015;
         public static final int phone_compatibility=0x7f0a0011;
         public static final int required_sensors=0x7f0a0014;
+        public static final int team_infinity=0x7f0a0024;
+        public static final int team_members_details=0x7f0a0026;
     }
     public static final class style {
         /** 
--- src/com/infinity/gam_o_sapien/EditKeysScreen.java
+++ src/com/infinity/gam_o_sapien/EditKeysScreen.java
@@ -1,17 +1,21 @@
 package com.infinity.gam_o_sapien;
 
+import android.app.Dialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class EditKeysScreen extends ActionBarActivity{
+public class EditKeysScreen extends ActionBarActivity {
 
 	private Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
 	private KeysStorage kStorageengine;
 	private char[] savedKeys;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,20 +34,75 @@ public class EditKeysScreen extends ActionBarActivity{
 		b7 = (Button) findViewById(R.id.b7);
 		b8 = (Button) findViewById(R.id.b8);
 		b9 = (Button) findViewById(R.id.b9);
-	
+
 		updateKeyPadUI();
-		
 
 		OnClickListener changeKeymethod = new OnClickListener() {
-			
+
+			private Dialog dialog;
+			private Button btn;
+
 			public void onClick(View clicked) {
-				Button b = (Button) clicked;
-				
-				b.setTextColor(Color.GREEN);
-				
+
+				btn = (Button) clicked;
+				// custom dialog
+				dialog = new Dialog(EditKeysScreen.this);
+				dialog.setContentView(R.layout.custom);
+				dialog.setTitle("Select Key");
+
+				final EditText keyEntered = (EditText) dialog
+						.findViewById(R.id.enteredKey);
+
+				Button ok = (Button) dialog.findViewById(R.id.dialogButtonOK);
+
+				Button cancel = (Button) dialog
+						.findViewById(R.id.dialogButtonCancel);
+				// if button is clicked, close the custom dialog
+				cancel.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						dialog.dismiss();
+					}
+				});
+				ok.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View arg0) {
+						String enteredCharacter = keyEntered.getText()
+								.toString().trim();
+						if (enteredCharacter.equals("")) {
+							dialog.setTitle("Renter !");
+						} else {
+							Log.d("INF", "char "+enteredCharacter);
+							btn.setTextColor(Color.GREEN);
+							switch(btn.getId())
+							{
+							case R.id.b1:kStorageengine.saveKey('0', enteredCharacter.charAt(0));break;
+							case R.id.b2:kStorageengine.saveKey('1', enteredCharacter.charAt(0));break;
+							case R.id.b3:kStorageengine.saveKey('2', enteredCharacter.charAt(0));break;
+							case R.id.b4:kStorageengine.saveKey('3', enteredCharacter.charAt(0));break;
+							case R.id.b5:kStorageengine.saveKey('4', enteredCharacter.charAt(0));break;
+							case R.id.b6:kStorageengine.saveKey('5', enteredCharacter.charAt(0));break;
+							case R.id.b7:kStorageengine.saveKey('6', enteredCharacter.charAt(0));break;
+							case R.id.b8:kStorageengine.saveKey('7', enteredCharacter.charAt(0));break;
+							case R.id.b9:kStorageengine.saveKey('8', enteredCharacter.charAt(0));break;
+							
+							}
+							
+							kStorageengine.readallKeys();
+							updateKeyPadUI();
+							dialog.dismiss();
+
+						}
+
+					}
+				});
+
+				dialog.show();
+
 			}
 		};
-		b1.setOnClickListener(changeKeymethod );
+		b1.setOnClickListener(changeKeymethod);
 		b2.setOnClickListener(changeKeymethod);
 		b3.setOnClickListener(changeKeymethod);
 		b4.setOnClickListener(changeKeymethod);
@@ -54,7 +113,7 @@ public class EditKeysScreen extends ActionBarActivity{
 		b9.setOnClickListener(changeKeymethod);
 
 	}
-	
+
 	private void updateKeyPadUI() {
 		b1.setText(savedKeys[0] + "");
 		b2.setText(savedKeys[1] + "");
@@ -67,8 +126,7 @@ public class EditKeysScreen extends ActionBarActivity{
 		b7.setText(savedKeys[6] + "");
 		b8.setText(savedKeys[7] + "");
 		b9.setText(savedKeys[8] + "");
-		
-		
+
 	}
-	
+
 }
